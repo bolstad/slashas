e08eaada0daf24fe2be9e10ff74966462c50c13d
Christian Bolstad <christian@carnaby.se>
2015-09-12 16:59:42 +0200

    also test by manipulating  variable

--- tests/EnviromentHTTPTest.php
+++ tests/EnviromentHTTPTest.php
@@ -7,7 +7,7 @@ use Slashas\Slashas;
 class EnviromentHTTPTest extends PHPUnit_Framework_TestCase {
   
 
-  public function testIsHTTPTrue() {
+  public function testMockedIsHTTPTrue() {
     $testo = new Slashas\Slashas;
 
     $stub = $this->getMockBuilder('DetectEnviroment')
@@ -21,7 +21,7 @@ class EnviromentHTTPTest extends PHPUnit_Framework_TestCase {
   }
 
 
-  public function testIsHTTPFalse() {
+  public function testMockedIsHTTPFalse() {
     $testo = new Slashas\Slashas;
 
     $stub = $this->getMockBuilder('DetectEnviroment')
@@ -32,6 +32,30 @@ class EnviromentHTTPTest extends PHPUnit_Framework_TestCase {
 
 	$testo->setEnvChecker( $stub );
     $this->assertFalse($testo->isHttp());  	
+  }
+
+  public function testSetEnvIsHTTPTrue() {
+    $testo = new Slashas\Slashas;
+
+    $env = new Slashas\DetectEnviroment;
+    $testo->setEnvChecker( $env );
+
+    $_GET['action'] = time();
+
+    $this->assertTrue($testo->isHttp());    
+
+    unset($_GET['action']);
+  }
+
+
+  public function testSetEnvIsHTTPFalse() {
+    $testo = new Slashas\Slashas;
+
+    $env = new Slashas\DetectEnviroment;
+    $testo->setEnvChecker( $env );
+
+
+    $this->assertFalse($testo->isHttp());   
   }
 
 
