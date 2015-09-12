12dbac528ee80d2ae2b2444f7ca830aaa2d17033
Christian Bolstad <christian@carnaby.se>
2015-09-12 19:11:54 +0200

    init our fetchers

--- src/Slashas/GetData.php
+++ src/Slashas/GetData.php
@@ -7,9 +7,39 @@ class GetData {
 
 	private $env = '';
 
+	private $fetcher = '';
 
 	public function setEnv( $env ) {
 		$this->env = $env;
+		$this->setupFetcher(); 
+	}	
+
+
+	public function setupFetcher() {
+
+		if ($this->env == 'slack') {
+#			$this->fetcher = new GetDataSlack();
+		}
+
+		if ($this->env == 'cli') {
+			$this->fetcher = new GetDataCli();
+		}
+
+		if ($this->env == 'http') {
+			$this->fetcher = new GetDataHttp();
+		}
+
+	}
+
+	public function getVariable( $var ) {
+
+		return $this->fetcher->getVariable( $var );
+
+	}
+
+
+	public function setDefaults( $data ) {
+		$this->fetcher->setDefaults( $data );
 	}
 
 }
