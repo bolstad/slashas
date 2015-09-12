63563488752b02634c124467857f4853cff9ce68
Christian Bolstad <christian@carnaby.se>
2015-09-12 16:32:36 +0200

    we like them phpdocs

--- src/Slashas/DetectEnviroment.php
+++ src/Slashas/DetectEnviroment.php
@@ -1,9 +1,13 @@
 <?php namespace Slashas\Slashas;
 
 
-class DetectEnviroment {
+class src/Slashas/Slashas.php {
 
 
+	/**
+	 * Detect if we are running in CLI (console) mode by inspecking the PHP_SAPI setting via php_sapi_name()
+	 * @return bool
+	 */
 	function isCli() {
 		if (php_sapi_name()=='cli')
 			return true;
@@ -12,17 +16,29 @@ class DetectEnviroment {
 	}
 
 
-
+	/**
+	 * Detect if we are running in HTTP (Web admin mode) by inspecting $_GET vars
+	 * @return bool
+	 */
 	function isHttp() {
 
+   	if (isset($_GET) && !empty($_GET['action'])) {
+			return true; 
+		} else {
+			return $false;
+		}
 	}
 
-
+	/**
+	 * Detect if we are running in Slack API mode by inspecting $_POST vars 
+	 * @return bool
+	 */
 	function isSlack() {
 
 	if (isset($_POST) && !empty($_POST['token'])) {
 			return true;
-		} else
+		} else {
 		return false; 
+		}
 	}
 }
